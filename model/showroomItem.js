eee0c1993f8a070e07ddecd8cf28a19693c72e48
Mee Ghel <mgf_21@icloud.com>
2026-01-06 00:14:46 +0800

    completed showroom

--- model/showroomItem.js
+++ model/showroomItem.js
@@ -1,5 +1,6 @@
 var db = require('./databaseConfig.js');
 
+// 1. GET ALL ITEMS
 module.exports.getShowroomItems = function (callback) {
     var conn = db.getConnection();
 
@@ -8,20 +9,29 @@ module.exports.getShowroomItems = function (callback) {
             return callback(err, null);
         }
 
-        var sql = 'SELECT * FROM itementity WHERE DTYPE = "FurnitureEntity"';
+
+        var sql = `
+            SELECT i.ID, i.NAME, i.CATEGORY, i.SKU, i.DESCRIPTION, 
+                   i._LENGTH, i.WIDTH, i.HEIGHT, 
+                   f.IMAGEURL, MAX(p.RETAILPRICE) as RETAILPRICE 
+            FROM itementity i 
+            JOIN furnitureentity f ON i.ID = f.ID 
+            LEFT JOIN item_countryentity p ON i.ID = p.ITEM_ID 
+            WHERE i.DTYPE = "FurnitureEntity" 
+            GROUP BY i.ID, i.NAME, i.CATEGORY, i.SKU, i.DESCRIPTION, i._LENGTH, i.WIDTH, i.HEIGHT, f.IMAGEURL
+        `;
 
         conn.query(sql, function (err, result) {
             conn.end();
-
             if (err) {
                 return callback(err, null);
             }
-
             return callback(null, result);
         });
     });
 };
 
+// 2. GET PROMOTIONS
 module.exports.getPromotions = function (callback) {
     var conn = db.getConnection();
 
@@ -34,16 +44,15 @@ module.exports.getPromotions = function (callback) {
 
         conn.query(sql, function (err, result) {
             conn.end();
-
             if (err) {
                 return callback(err, null);
             }
-
             return callback(null, result);
         });
     });
 };
 
+// 3. GET ITEMS BY ROOM (SMART FILTER)
 module.exports.getShowroomItemsByRoom = function (room, callback) {
     var conn = db.getConnection();
 
@@ -52,20 +61,47 @@ module.exports.getShowroomItemsByRoom = function (room, callback) {
             return callback(err, null);
         }
 
+        var categories = [];
+
+        // Define which categories belong to which room
+        if (room === 'Living Room') {
+            categories = ['Sofas & Chair', 'Tables & Desks', 'Lightings'];
+        } 
+        else if (room === 'Bedroom') {
+            categories = ['Beds & Mattresses', 'Lightings', 'Cabinets & Storage'];
+        } 
+        else if (room === 'Study') {
+            categories = ['Tables & Desks', 'Cabinets & Storage', 'Lightings', 'Study','Sofas & Chair'];
+        } 
+        else if (room === 'Kitchen') {
+             categories = ['Tables & Desks', 'Cabinets & Storage']; 
+        }
+        else {
+            categories = [room];
+        }
+
+      
+        var placeholders = categories.map(() => '?').join(',');
+
+        
         var sql = `
-            SELECT *
-            FROM itementity
-            WHERE DTYPE = 'FurnitureEntity'
-            AND category = ?
+            SELECT i.ID, i.NAME, i.CATEGORY, i.SKU, i.DESCRIPTION, 
+                   i._LENGTH, i.WIDTH, i.HEIGHT, 
+                   f.IMAGEURL, MAX(p.RETAILPRICE) as RETAILPRICE 
+            FROM itementity i 
+            JOIN furnitureentity f ON i.ID = f.ID 
+            LEFT JOIN item_countryentity p ON i.ID = p.ITEM_ID 
+            WHERE i.DTYPE = "FurnitureEntity" 
+            AND i.CATEGORY IN (${placeholders}) 
+            GROUP BY i.ID, i.NAME, i.CATEGORY, i.SKU, i.DESCRIPTION, i._LENGTH, i.WIDTH, i.HEIGHT, f.IMAGEURL
         `;
 
-        conn.query(sql, [room], function (err, result) {
+        conn.query(sql, categories, function (err, result) {
             conn.end();
-
             if (err) {
+                console.log("DB Error:", err);
                 return callback(err, null);
             }
-
             return callback(null, result);
         });
     });
